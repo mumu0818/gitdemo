9ebbdc9fec18b25526c29dde2240bc2d5778912f
mumu <mumu081834@163.com>
2019-10-30 10:59:44 +0800

    for

---
+++ git2/src/main/java/com/test/Test.java
@@ -0,0 +1,18 @@
+package com.test;
+
+/**
+ * @author 木木
+ * @version 1.0.0
+ * @ClassName Test.java
+ * @createTime 2019年10月30日 10:59:00
+ * @Description TODO
+ */
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println(i);
+
+        }
+    }
+}
