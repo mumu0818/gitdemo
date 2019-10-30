8b0d7f17f8402587d55d73baf42c5de9c9d01e96
mumu <mumu081834@163.com>
2019-10-30 11:29:43 +0800

    test

---
+++ gitee/src/main/java/com/Test.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * @author 木木
+ * @version 1.0.0
+ * @ClassName Test.java
+ * @createTime 2019年10月30日 11:29:00
+ * @Description TODO
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
