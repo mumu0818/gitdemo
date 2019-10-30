0f35df488f4805a328390e5cb91746ab94177b2c
mumu <mumu081834@163.com>
2019-10-30 11:26:53 +0800

    git3

---
+++ git3/src/main/java/First.java
@@ -0,0 +1,14 @@
+/**
+ * @author 木木
+ * @version 1.0.0
+ * @ClassName First.java
+ * @createTime 2019年10月30日 11:25:00
+ * @Description TODO
+ */
+public class First {
+
+    public static void main(String[] args) {
+
+        System.out.println("连接已有项目的github");
+    }
+}
