78899025ffa5d9ab8fb38a38dea58a7231e342fd
mumu <mumu081834@163.com>
2019-10-30 14:48:45 +0800

    test

--- git4/src/main/java/com/Test.java
+++ git4/src/main/java/com/Test.java
@@ -4,11 +4,12 @@ package com;
  * @author 木木
  * @version 1.0.0
  * @ClassName Test.java
- * @createTime 2019年10月30日 11:29:00
+ * @createTime 2019年10月30日 14:47:00
  * @Description TODO
  */
 public class Test {
+
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("0818");
     }
 }
