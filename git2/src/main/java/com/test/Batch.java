1da1a2cf21ab4cc92cbb7045631a6e2bba53c0f9
mumu <mumu081834@163.com>
2019-10-30 11:19:51 +0800

    github

---
+++ git2/src/main/java/com/test/Batch.java
@@ -0,0 +1,15 @@
+package com.test;
+
+/**
+ * @author 木木
+ * @version 1.0.0
+ * @ClassName Batch.java
+ * @createTime 2019年10月30日 11:06:00
+ * @Description TODO
+ */
+public class Batch {
+
+    public void test(){
+        System.out.println("test");
+    }
+}
